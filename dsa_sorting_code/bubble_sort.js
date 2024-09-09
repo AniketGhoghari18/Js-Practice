056318fdb3398a07d5df00f9b2d56f266944a542
AniketGhoghari18 <aniket.ghoghari@scriptalldna.com>
2024-09-09 18:02:50 +0530

    Add bubble sort logix

--- dsa_sorting_code/bubble_sort.js
+++ dsa_sorting_code/bubble_sort.js
@@ -2,5 +2,14 @@ const array = [47, 12, 18, 18, 23, 39, 1, 50, 44, 16];
 
 function bubble_sort(array)
 {
-  
+  for (let i = 0; i < array.length; i++) {
+     for (let j = 0; j < array.length - 1 - i; j++) {
+        if (array[j] > array[j+1]) {
+            [array[j] , array[j+1]] = [array[j+1],array[j]]
+        }
+     }
+  }
 }
+
+bubble_sort(array);
+console.log(array);
