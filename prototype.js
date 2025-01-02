030ca4292c5276f17b7fffd48af497eb2f240485
AniketGhoghari18 <aniket.ghoghari@scriptalldna.com>
2025-01-02 12:24:49 +0530

    update prototype filed and delete bind file

--- prototype.js
+++ prototype.js
@@ -1,3 +1,7 @@
+
+// In Javascript , a Prototype is an object that other object can inherit properties and methods from...
+// Every JavaScript object has a prototype, and when an object is created, it inherits the properties and methods of its prototype.
+// Access prototype using __proto__ or getPrototypeOf
 class person{
     constructor(first_name , last_name , age )
     {
@@ -14,5 +18,6 @@ class person{
  }
 
  const person1 = new person('Aniket' , 'Ghoghari' ,22 );
-//  console.log(person1.__proto__.introduction.bind(person1)());
- console.log(person1.introduction());
+//  console.log(person1.__proto__.introduction.bind(person1)()); // Using __proto__
+//  console.log(Object.getPrototypeOf(person1).introduction.bind(person1)()); // using getPrototypeOf
+ console.log(person1.introduction()); // Js Automatic find in prototpye
